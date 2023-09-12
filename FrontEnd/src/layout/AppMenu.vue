9cd26c629e06d6ce630e21b1b3e42f7dfa1c0045
rifa <rifadianekaf@gmail.com>
2023-09-12 16:58:42 +0700

    edit menu nasabah

--- FrontEnd/src/layout/AppMenu.vue
+++ FrontEnd/src/layout/AppMenu.vue
@@ -33,83 +33,11 @@ const model = ref([
     {
         label: 'Nasabah',
         items: [
-            { label: 'Cek Saldo', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Setor', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Ambil', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-            { label: 'Transaksi Antar Rekening', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Bayar Telpon', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-            { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-        ]
-    },
-    {
-        label: 'Prime Blocks',
-        items: [
-            { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: 'NEW' },
-            { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: 'https://www.primefaces.org/primeblocks-vue', target: '_blank' }
-        ]
-    },
-    {
-        label: 'Utilities',
-        items: [
-            { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },
-            { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
-        ]
-    },
-    {
-        label: 'Pages',
-        icon: 'pi pi-fw pi-briefcase',
-        to: '/pages',
-        items: [
-            {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
-            },
-            {
-                label: 'Auth',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'Login',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/auth/login'
-                    },
-                    {
-                        label: 'Error',
-                        icon: 'pi pi-fw pi-times-circle',
-                        to: '/auth/error'
-                    },
-                    {
-                        label: 'Access Denied',
-                        icon: 'pi pi-fw pi-lock',
-                        to: '/auth/access'
-                    }
-                ]
-            },
-            {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/pages/crud'
-            },
-            {
-                label: 'Timeline',
-                icon: 'pi pi-fw pi-calendar',
-                to: '/pages/timeline'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/pages/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/pages/empty'
-            }
+            { label: 'Cek Saldo', icon: 'pi pi-wallet', to: '/uikit/list' },
+            { label: 'Setor', icon: 'pi pi-dollar', to: '/uikit/tree' },
+            { label: 'Ambil', icon: 'pi pi-money-bill', to: '/uikit/panel' },
+            { label: 'Transfer', icon: 'pi pi-arrow-right-arrow-left', to: '/uikit/CekSaldoComponent' },
+            { label: 'Bayar Telpon', icon: 'pi pi-mobile', to: '/uikit/media' }
         ]
     },
     {
@@ -156,27 +84,6 @@ const model = ref([
             }
         ]
     },
-    {
-        label: 'Get Started',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-question',
-                to: '/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-search',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
-            },
-            {
-                label: 'Nuxt Version',
-                url: 'https://github.com/primefaces/sakai-nuxt',
-                icon: 'pi pi-fw pi-star'
-            }
-        ]
-    }
 ]);
 </script>
 
