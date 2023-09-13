b928328f301036619206d00090a21772e5312525
arhand98 <timotius_darmawan@outlook.com>
2023-09-13 15:13:01 +0700

    new file:   BackEnd/src/controllers/RoleMenuController.ts 	modified:   BackEnd/src/db/models/role_menu.js 	modified:   BackEnd/src/index.ts 	new file:   BackEnd/src/routers/RoleMenuRoutes.ts

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -14,48 +14,50 @@ import RoleRoutes from './routers/RoleRoutes';
 import MasterBankRoutes from './routers/MasterBankRoutes';
 import HakAksesRoutes from './routers/HakAksesRoutes';
 import MenuRoutes from './routers/MenuRoutes';
+import RoleMenuRoutes from './routers/RoleMenuRoutes';
 
 class App {
-    public app: Application;
+  public app: Application;
 
-    constructor() {
-        this.app = express();
-        this.plugins();
-        this.routes();
-        dotenv();
-    }
+  constructor() {
+    this.app = express();
+    this.plugins();
+    this.routes();
+    dotenv();
+  }
 
-    protected plugins(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(morgan('dev'));
-        this.app.use(compression());
-        this.app.use(helmet());
-        this.app.use(cors());
-    }
+  protected plugins(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(morgan('dev'));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
 
-    protected routes(): void {
-        this.app.route('/').get((req, res) => {
-            res.send('ini route menggunakan TypeScript');
-        });
+  protected routes(): void {
+    this.app.route('/').get((req, res) => {
+      res.send('ini route menggunakan TypeScript');
+    });
 
-        this.app.route('/users').post((req, res) => {
-            res.send(req.body);
-        });
+    this.app.route('/users').post((req, res) => {
+      res.send(req.body);
+    });
 
-        this.app.use('/api/v1/admin/manage/users', UserRoutes);
-        this.app.use('/api/v1/auth', AuthRoutes);
-        this.app.use('/api/v1/todos', TodoRoutes);
-        this.app.use("/api/v1/masterBank", MasterBankRoutes);
-        this.app.use('/api/v1/admin/manage/roles', RoleRoutes);
-        this.app.use('/api/v1/admin/manage/menus', MenuRoutes);
-        this.app.use('/api/v1/admin/manage/hakAkses', HakAksesRoutes);
-    }
+    this.app.use('/api/v1/admin/manage/users', UserRoutes);
+    this.app.use('/api/v1/auth', AuthRoutes);
+    this.app.use('/api/v1/todos', TodoRoutes);
+    this.app.use('/api/v1/masterBank', MasterBankRoutes);
+    this.app.use('/api/v1/admin/manage/roles', RoleRoutes);
+    this.app.use('/api/v1/admin/manage/menus', MenuRoutes);
+    this.app.use('/api/v1/admin/manage/hakAkses', HakAksesRoutes);
+    this.app.use('/api/v1/admin/manage/roleMenu', RoleMenuRoutes);
+  }
 }
 
 const port: number = 8000;
 const app = new App().app;
 app.listen(port, () => {
-    console.log('Aplikasi ini berjalan di port ' + port);
+  console.log('Aplikasi ini berjalan di port ' + port);
 
-    console.log(process.env.DB_USER);
+  console.log(process.env.DB_USER);
 });
