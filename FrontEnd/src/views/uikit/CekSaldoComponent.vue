ab440a703d555b4eaf77cfc65c00ecf7d861b667
lilisk <liliskurnia548@gmail.com>
2023-09-14 09:27:35 +0700

    be history, fe cek saldo belum fix

--- FrontEnd/src/views/uikit/CekSaldoComponent.vue
+++ FrontEnd/src/views/uikit/CekSaldoComponent.vue
@@ -1,79 +1,88 @@
 <template>
     <div class="grid p-fluid">
         <div class="col-12">
-        <div class="card">
-            <h2>Cek Saldo</h2>
-            <form @submit="cekSaldo">
-            <div>
-                <div>
-                <h5>No. Rekening:</h5>
-                <InputText type="text" v-model="nomorRekening" class="custom-input" :class="{'p-invalid': nomorRekeningError}" required /><br />&nbsp;
-                <span v-if="nomorRekeningError" class="p-error">Nomor rekening harus diisi</span>
-                <br />&nbsp;
-                <Button label="Submit" class="custom-button" type="submit" />
+            <div class="card">
+                <h2>Cek Saldo</h2>
+                <form @submit.prevent="getSaldo">
+                    <div>
+                        <div>
+                            <h5>No. Rekening:</h5>
+                            <InputText type="number" v-model="norek" class="custom-input"
+                                :class="{ 'p-invalid': nomorRekeningError }" required /><br />&nbsp;
+                            <span v-if="nomorRekeningError" class="p-error">Nomor rekening harus diisi</span>
+                            <br />&nbsp;
+                            <Button label="Submit" class="custom-button" type="submit" />
+                        </div>
+                    </div>
+                </form>
             </div>
-            </div>
-        </form>
         </div>
-    </div>
     </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import Swal from 'sweetalert2';
+<script>
+// import axios
+import axios from "axios";
+export default {
+    data() {
+        return {
+            norek: "",
+            nasabah: [],
+        };
+    },
+    created: function () {
+        this.getSaldo()
+    },
+    methods: {
+        async getSaldo() {
+            console.log("cek ini", this.norek);
+            try {
+                const response = await axios.get(
+                    `http://localhost:8000/api/v1/nasabah/cekSaldo`, 
+                    {
+                        norek: Number(this.norek)
+                    }
+                );
+                this.nasabah = response.data;
 
-const nomorRekening = ref('');
-const nomorRekeningError = ref (false);
+                if (!norek.value) {
+                    nomorRekeningError.value = true;
+                    return;
+                } else {
+                    nomorRekeningError.value = false;
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        },
 
-const cekSaldo = (e) => {
-    e.preventDefault(); // Mencegah refresh halaman default
+        async cancelProcess() {
+            this.$router.push("/uikit/CekSaldoComponent");
+        },
 
-    // Validasi input nomor rekening
-    if (!nomorRekening.value) {
-    nomorRekeningError.value = true;
-    return; // Menghentikan eksekusi jika input kosong
-} else {
-    nomorRekeningError.value = false;
-}
-
-    const nomorRekeningDummy = '1234567890'; // Nomor rekening dummy
-    const saldoDummy = 5000; // Saldo dummy
-
-    if (nomorRekening.value === nomorRekeningDummy) {
-    Swal.fire({
-        title: 'Saldo Anda',
-        text: `Saldo untuk nomor rekening ${nomorRekeningDummy} saat ini adalah: Rp.${saldoDummy},00`,
-        icon: 'success',
-    });
-} else {
-    Swal.fire({
-        title: 'Nomor Rekening Tidak Ditemukan',
-        text: 'Nomor rekening tidak ditemukan.',
-        icon: 'error',
-    });
-}
+    },
 };
 </script>
 
 <style>
 .custom-input {
     width: 200px;
-    height: 40px; 
-    font-size: 16px; 
+    height: 40px;
+    font-size: 16px;
 }
 
 .custom-button {
-    width: 100px; 
-    height: 40px; 
+    width: 100px;
+    height: 40px;
     font-size: 16px;
 }
 
 .p-invalid {
-  border-color: red; /* Warna border merah jika input tidak valid */
+    border-color: red;
+    /* Warna border merah jika input tidak valid */
 }
 
 .p-error {
-  color: red; /* Warna teks merah untuk pesan kesalahan */
-}
-</style>
+    color: red;
+    /* Warna teks merah untuk pesan kesalahan */
+}</style>
